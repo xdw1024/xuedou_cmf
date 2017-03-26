4dbca5fb262327474f9e24f4bbe8fea21d15b3a4
xudongwei08 <843617835@qq.com>
2017-03-26 12:43:32 +0800

    项目初始化

---
+++ data/conf/db.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    //'DB_HOST' => '119.29.227.90',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'xuedou_cmf',
+    'DB_USER' => 'root',
+    //'DB_PWD' => 'xdw',
+    'DB_PWD' => 'root',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'cmf_',
+    //密钥
+    "AUTHCODE" => 'D1AQlSi3m5O4IPM6qB',
+    //cookies
+    "COOKIE_PREFIX" => 'Ck0AHv_',
+);
